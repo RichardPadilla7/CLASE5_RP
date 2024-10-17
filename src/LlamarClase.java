cc2fb74328a30c7c1d7d6a22c41c2129a8b4d79d
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-10-17 08:58:08 -0500

    1 commit

--- src/LlamarClase.java
+++ src/LlamarClase.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class LlamarClase {
+    public static void main(String[] args) {
 
-public class Llamar {
+        Vehiculos auto1 = new Vehiculos("Ferrari", "Azul", 2012,"PDH-123", 6000);
+        auto1.infoVehiculos();
+
+        Vehiculos auto2 = new Vehiculos("Ferrari");
+        auto2.modelado();
+
+        Vehiculos auto3 = new Vehiculos();
+        auto3.infoVehiculosVACIO();
+    }
 }
--- src/Vehiculos.java
+++ src/Vehiculos.java
@@ -1,9 +1,58 @@
-public class Main {
-    public static void main(String[] args) {
+public class Vehiculos {
 
-        System.out.println();
+    String modelo,color,placa;
+    double precio;
+    int anio;
+
+    public Vehiculos(String modelo, String color, int anio, String placa, double precio){
+        this.modelo = modelo;
+        this.color = color;
+        this.anio = anio;
+        this.placa = placa;
+        this.precio = precio;
+    }
+    public void infoVehiculos(){
+        System.out.println("Placa: " + placa);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Color: " + color);
+        System.out.println("Año: " + anio);
+    }
 
 
 
+
+
+//COnstructor vacio
+    public Vehiculos(){
+        this.modelo = "Ferrari";
+        this.color = "Azul";
+        this.anio = 2012;
+        this.placa = "PSI-345";
+        this.precio = 6000;
+    }
+    public void infoVehiculosVACIO(){
+        System.out.println("Placa: " + placa);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Color: " + color);
+        System.out.println("Año: " + anio);
+    }
+
+
+
+
+
+
+
+
+
+//SOLO UN ATRIBUTO
+    public Vehiculos(String modelo){
+        this.modelo = modelo;
+    }
+    public void modelado(){
+        System.out.println("");
+        System.out.println("El modelo es: " + modelo);
     }
 }
